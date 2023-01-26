9c2f06e336fb22cf13080093671521a64aafe675
Alexander Ignatev <alexign473@gmail.com>
2023-01-26 21:49:50 +0300

    Add button to remove book Refactor rendering of books

--- js/app.js
+++ js/app.js
@@ -12,6 +12,10 @@ function addBook(book) {
   myLibrary.push(book);
 }
 
+function removeBook(index) {
+  myLibrary.splice(index, 1);
+}
+
 const book1 = new Book('Witcher', 'A. Sapkovsky', 123, false);
 const book2 = new Book('Headfirst JS', 'O. Reily', 999, true);
 addBook(book1);
@@ -19,26 +23,57 @@ addBook(book2);
 
 const booksGrid = document.querySelector('.cards-grid');
 
-function getCardElement(book) {
-  const card = document.createElement('div');
-  card.className = 'card rounded-3 shadow-sm';
-  const cardBody = document.createElement('div');
-  cardBody.className = 'card-body fs-5';
-  cardBody.textContent = `${book.title} by ${book.author}`;
-  card.append(cardBody);
-  return card;
-}
-
 function renderData() {
   booksGrid.innerHTML = '';
-  myLibrary.forEach((book) => {
-    const card = getCardElement(book);
-    booksGrid.append(card);
-  });
+
+  const html = myLibrary
+    .map(
+      (book, index) => `
+      <div class="card rounded-3 shadow-sm">
+        <div class="card-body fs-5">
+          <h3 class="card-title">${book.title}</h3>
+          <p>by ${book.author}</p>
+          <p>Pages: ${book.pages}</p>
+          <span>Status: ${book.isRead ? 'Read' : 'Not read'}</span>
+        </div>
+        <div class="card-footer bg-transparent border-top-0">
+          <input
+            type="checkbox"
+            class="btn-check"
+            id="check-${index}"
+            autocomplete="off"
+          />
+          <label
+            class="w-100 mb-3 btn btn-lg btn-outline-success"
+            for="check-${index}"
+            >Read</label
+          ><br />
+          <button
+            type="button"
+            class="w-100 btn btn-lg btn-secondary"
+            data-remove="${index}"
+          >
+            Remove
+          </button>
+        </div>
+      </div>`
+    )
+    .join(' ');
+
+  booksGrid.innerHTML = html;
 }
 
+function handleDelete(e) {
+  if (!e.target.dataset.remove) return;
+  const index = +e.target.dataset.remove;
+  removeBook(index);
+  renderData();
+}
+
+booksGrid.addEventListener('click', handleDelete);
+
 const btn = document.querySelector('[data-bs-toggle="modal"]');
-btn.click();
+// btn.click();
 
 function submitForm(e) {
   e.preventDefault();
