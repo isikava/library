7309302d97300f2430fab510a22d564ac3ef2796
Alexander Ignatev <alexign473@gmail.com>
2023-01-26 20:12:29 +0300

    Add submit new Book

--- js/app.js
+++ js/app.js
@@ -16,19 +16,45 @@ const book1 = new Book('Witcher', 'A. Sapkovsky', 123, false);
 const book2 = new Book('Headfirst JS', 'O. Reily', 999, true);
 addBook(book1);
 addBook(book2);
-addBook();
-
-// console.table(myLibrary);
 
 const booksGrid = document.querySelector('.cards-grid');
 
-myLibrary.forEach((book, i) => {
-  console.log(i);
+function getCardElement(book) {
   const card = document.createElement('div');
   card.className = 'card rounded-3 shadow-sm';
   const cardBody = document.createElement('div');
   cardBody.className = 'card-body fs-5';
   cardBody.textContent = `${book.title} by ${book.author}`;
   card.append(cardBody);
-  booksGrid.append(card);
-});
+  return card;
+}
+
+function renderData() {
+  booksGrid.innerHTML = '';
+  myLibrary.forEach((book) => {
+    const card = getCardElement(book);
+    booksGrid.append(card);
+  });
+}
+
+const btn = document.querySelector('[data-bs-toggle="modal"]');
+btn.click();
+
+function submitForm(e) {
+  e.preventDefault();
+  const { target } = e;
+
+  const formData = new FormData(target);
+  const { title, author, pages } = Object.fromEntries(formData);
+  const cb = target.querySelector('#isRead');
+
+  const book = new Book(title, author, pages, cb.checked);
+  addBook(book);
+  renderData();
+}
+
+const addBookForm = document.querySelector('#addBookForm');
+
+addBookForm.addEventListener('submit', submitForm);
+
+renderData();
